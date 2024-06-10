c3a38f7a1c3d6b2ee590e1fce34f354f01a1cb99
Vaibhav Patil <vaibhav.chintaman.patil@gmail.com>
2024-06-10 21:53:52 +0530

    Notify user that request to root URL is invalid.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "Root URL"
+    try:
+        return jsonify({"error":"Not a valid API endpoint."}), 405
+    except Exception as e:
+        return jsonify({"error":"Internal server error."}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

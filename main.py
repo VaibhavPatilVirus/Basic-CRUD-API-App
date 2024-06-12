2c459332cbf2af5033cfb61e2459db64f7779318
Vaibhav Patil <vaibhav.chintaman.patil@gmail.com>
2024-06-12 18:36:18 +0530

    Provide env variable for app host.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src import app
 import os
 
+#Start the server.
 if __name__=="__main__":
     db_uri = os.getenv('DATABASE_URL')
     app_port = os.getenv('APP_PORT')
+    host = os.getenv('APP_HOST')
     flaskApp = app.create_app(db_uri)
-    flaskApp.run("0.0.0.0", port=app_port, debug=False)
+    flaskApp.run(host, port=app_port, debug=False)
